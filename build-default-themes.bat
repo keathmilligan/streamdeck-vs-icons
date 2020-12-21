199828cd017e9faf78d95aa4a87bc44e4f132d26
Keath Milligan <keath@keathmilligan.net>
2020-12-20 20:25:14 -0600

    New icons for Dart, Flutter, Android and more

--- build-default-themes.bat
+++ build-default-themes.bat
@@ -3,4 +3,4 @@
 
 call .\build.bat "Blue" "#4090e8" "#e0e020" "#e02020" "#202020"
 call .\build.bat "Green" "#20a038" "#f0f0f0" "#e02020" "#202020"
-call .\build.bat "White" "#b2b8b9" "#0063bc" "#e02020" "#202020"
+call .\build.bat "White" "#f0f0f0" "#0063bc" "#e02020" "#202020"
